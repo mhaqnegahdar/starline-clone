f0c5133f4cdb296a05b05ec4df97cd14471b3640
max jn <maxjn763@gmail.com>
2023-07-03 11:15:42 +0330

    Responsive Header: - Made Header to ge responsive - Added **OffmenuItem** component - Added **types** directory - Added **Navbar Styles**

--- src/components/common/layout/header/Header.tsx
+++ src/components/common/layout/header/Header.tsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 //Components
 import InfoBar from "./InfoBar";
 import NavBar from "./NavBar";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <header className="header--background px-2 px-md-3 px-lg-4 p-0 m-0 pt-2">
-      <InfoBar />
-      <NavBar />
+      <InfoBar toggle={toggle} setToggle={setToggle} />
+      <NavBar toggle={toggle} setToggle={setToggle} />
     </header>
   );
 };
--- src/components/common/layout/header/InfoBar.tsx
+++ src/components/common/layout/header/InfoBar.tsx
@@ -4,11 +4,15 @@ import Link from "next/link";
 // Icons
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { HiLocationMarker } from "react-icons/hi";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
+// Toggle
+import { ToggleStateType } from "@/types";
 
-const InfoBar = () => {
+const InfoBar = ({ toggle, setToggle }: ToggleStateType) => {
   return (
     <>
-      <div className="d-flex justify-content-between align-items-center px-5  py-3">
+      <div className=" infobar d-flex justify-content-between align-items-center  p-0 m-0 pt-2 pt-md-3 px-lg-5">
         <Link href={"#"}>
           <Image
             src={`https://image123.azureedge.net/houseofcars/9168930046748802-9391419316301419-photo_2023-06-08_23-34-49-removebg-preview321.png`}
@@ -18,6 +22,7 @@ const InfoBar = () => {
             alt="Logo"
           />
         </Link>
+        {/* Contact Info */}
         <div className="info d-flex gap-4 d-none d-lg-flex">
           <div className=" d-flex justify-content-center align-items-center gap-2 ">
             <HiLocationMarker size={17} className="text-danger" />
@@ -27,6 +32,17 @@ const InfoBar = () => {
             <BsFillTelephoneFill size={17} className="text-danger" />
             <p>905-782-5159</p>
           </div>
+        </div>
+        {/* Toggle Info */}
+        <div
+          className="infobar__burgur-icon d-block d-lg-none  "
+          onClick={() => setToggle(p => (p == false ? true : false))}
+        >
+          {toggle === false ? (
+            <RxHamburgerMenu size={30} />
+          ) : (
+            <AiOutlineClose size={30} />
+          )}
         </div>
       </div>
     </>
